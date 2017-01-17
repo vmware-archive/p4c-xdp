47660192040b5175ebbbe4748d57b6ca39cf0246
William Tu <u9012063@gmail.com>
2017-01-17 14:06:32 -0800

    bpfloader: make testing easier
    Add bpfloader so that through syscall, we can load and verify the
    P4-generated ebpf program, without actually attaching to tc or XDP.

---
+++ tests/load_and_verify.c
@@ -0,0 +1,38 @@
+#include <linux/bpf.h>
+#include <assert.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/resource.h>
+
+#include "bpf_load.h"
+#include "libbpf.h"
+
+int main(int ac, char **argv)
+{
+	char filename[256];
+	struct rlimit r = {RLIM_INFINITY, RLIM_INFINITY};
+
+	snprintf(filename, sizeof(filename), "%s", argv[1]);
+
+    if (setrlimit(RLIMIT_MEMLOCK, &r)) {
+		perror("setrlimit(RLIMIT_MEMLOCK, RLIM_INFINITY)");
+		return 1;
+	}
+
+	if (ac != 2) {
+		printf("usage: %s BPF.o\n", argv[0]);
+		return 1;
+	}
+
+	if (load_bpf_file(filename)) {
+		printf("FAILED: %s", bpf_log_buf);
+		return 1;
+	}
+	printf("PASS\n");
+
+	return 0;
+}
