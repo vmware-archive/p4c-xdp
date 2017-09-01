91b0f94772cb69c98aae3e819557dcb09964e5f5
Mihai Budiu <mbudiu@vmware.com>
2017-08-31 18:23:10 -0700

    Modify makefile to run some xdp tests during make check (#52)

---
+++ run-p4c-xdp.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+folder="$1"
+verbose=0
+shift
+
+while getopts "bvf" opt; do
+    case $opt in
+        v)
+            verbose=1
+            ;;
+    esac
+    shift
+done
+
+file="$1"
+
+if [ $verbose -eq "1" ]; then
+    echo $folder/build/p4c-xdp $file
+fi
+$folder/build/p4c-xdp -o tmp.c $file
